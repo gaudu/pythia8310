340a99e61b040937605db1d60d6f641e951c98b6
Chloé Gaudu <95409748+gaudu@users.noreply.github.com>
2023-12-22 14:15:55 +0100

    implement Pythia example to run pi- C collisions at 158 & 350 GeV/c + analyze using Rivet (NA61SHINE_2022_I2155140)

---
+++ examples/main1003.cc
@@ -0,0 +1,67 @@
+// Authors: Marius Utheim <marius.m.utheim@jyu.fi>;
+//          Chloé Gaudu <gaudu@uni-wuppertal.de>
+
+// Keywords: analysis; rivet; hA collisions
+
+// This example simulates events for specific particle beam, energy and frame settings.
+// These events are analyzed using Rivet, set to ignore messages regarding beam 
+// properties. The Rivet configuration includes adding a specific analysis and specifying
+// the YODA output file. A log file is printed with Pythia statistics after the run.
+
+#include "Pythia8/HeavyIons.h"
+#include "Pythia8/Pythia.h"
+#include "Pythia8Plugins/Pythia8Rivet.h"
+
+using namespace Pythia8;
+
+int main() {
+
+  // Run settings.
+  int nEvents = 1000;
+  constexpr int nCases = 2; 
+  vector<string> caseLabels = { "158", "350" };
+  
+  for (int iCase = 0; iCase < nCases; ++iCase) {
+
+    // Pythia configuration.
+    Pythia pythia;
+    pythia.readString("Beams:idA = -211");
+    pythia.readString("Beams:idB = 1000060120");
+    pythia.readString("Beams:frameType = 2");
+    if (iCase == 0)
+      pythia.readString("Beams:eA = 158.");
+    else
+    	pythia.readString("Beams:eA = 350.");
+    pythia.readString("Beams:eB = 0.93827"); // mass_proton ~= mass_carbon/12
+
+    // Parameters to reuse initialization.
+    pythia.readString("MultipartonInteractions:reuseInit = 3");
+    pythia.readString("HeavyIon:SasdMpiReuseInit = 3");
+    pythia.readString("HeavyIon:SigFitReuseInit = 3");
+    string initPrefix = "main1003-"+caseLabels[iCase];
+    pythia.readString("MultipartonInteractions:initFile = "+initPrefix+".mpi");
+    pythia.readString("HeavyIon:SasdMpiInitFile = "+initPrefix+".sasd.mpi");
+    pythia.readString("HeavyIon:SigFitInitFile = "+initPrefix+".sigfit");
+
+    if (!pythia.init()) {
+      cout << "Pythia failed to initialize." << endl;
+      return -1;
+    }
+
+    // Rivet initialization.
+    Pythia8Rivet rivet(pythia, "main1003.yoda");
+    rivet.ignoreBeams(true);
+    rivet.addAnalysis("NA61SHINE_2022_I2155140");
+
+    // Loop over events.
+    for ( int iEvent = 0; iEvent < nEvents; ++iEvent ) { 
+      if (!pythia.next()) continue;
+      rivet();
+    }
+    rivet.done();
+    pythia.stat();
+
+  }
+
+  return 0;
+}
